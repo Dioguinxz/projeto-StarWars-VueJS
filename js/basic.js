41ead7e0544e87e15a1cebae066f901784833251
Dioguinxz <doassis@hotmail.com.br>
2024-09-13 16:56:35 -0300

    First commit

---
+++ js/basic.js
@@ -0,0 +1,37 @@
+console.log('ola java')
+
+var userName = 'Diogo'
+document.getElementById('user-name').innerHTML = userName;
+
+// var nome = 'Mestre Yoda'
+// var idade = 100
+// var jedi = false
+
+// console.log(typeof nome)
+// console.log(typeof idade)
+// console.log(typeof jedi)
+
+// function soma(n1, n2) {
+//     var resultado = n1 + n2;
+//     console.log(resultado)
+// }
+
+// soma(3, 6)
+
+
+
+// function boasVindas(nome) {
+// alert(nome + ', Seja bem vindo')
+// }
+
+// boasVindas('Diogo')
+
+
+// function boasVindas(sexo, nome) {
+//     if (sexo == 'masculino') {
+//         alert(nome + ', seja bem vindo!')
+//     } else
+//         alert(nome + ', seja bem vinda!')
+// }
+// boasVindas('masculino', 'Diogo')
+
